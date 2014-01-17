e91cc56b7d52ddf71587a4bcd47d8ab8f6ccf182
Dennis Keck <dennis@linux-99ta.site>
2014-01-17 10:21:57 +0100

    Added test code for communication via ROS

---
+++ testcode_crazyflie_thrust_controller.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python2.7
+import rospy
+import time
+import threading
+import logging
+
+from std_msgs.msg import UInt16
+
+logger = logging.getLogger()
+
+INITIAL_ROLL = 0.0
+INITIAL_PITCH = 0.0
+INITIAL_YAWRATE = 0
+INITIAL_THRUST = 10001
+
+class CrazyDemo(object):
+
+    def start(self):
+        import sys
+        range = 350
+        factor = 1000
+        thrust = INITIAL_THRUST
+	pub = rospy.Publisher('thrust', UInt16)
+	rospy.init_node('crazyflie')
+        thrust = thrust + 20000
+        while not rospy.is_shutdown():
+            ch = sys.stdin.read(1)    
+            if ch == "u":
+                thrust = thrust + factor
+            elif ch == "e":
+                thrust = 0
+                self.stop();
+            elif ch == "d":
+                thrust = thrust - factor
+            rospy.loginfo(str)
+	    pub.publish(thrust)
+	    rospy.sleep(0.05)
+
+
+
+c = CrazyDemo()
+c.start()
+time.sleep(1)
+c.stop()
+
+
