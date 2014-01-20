71a153c736fe23d52878004bc44449c0c004193e
linoba <dominik.kiefer@t-online.de>
2014-01-20 16:15:35 +0100

    crazyflie_uri - commenting done

--- crazyflie_uri.py
+++ crazyflie_uri.py
@@ -19,31 +19,74 @@ from std_msgs.msg import String
 logging.basicConfig(level=logging.DEBUG)
 
 class CrazyflieURI:
+    """
+    Class for finding crazyflies and saving their connection-information.
+    It will allow to find, add, find and add, print and modify all found
+    information and connection-data.
+    Partially the execution will be interactive/ require interaction.
+    """
     def __init__(self):
+        """
+        Creating and empty list for uri connection-data.
+        The amount of found links is set to zero.
+        """
         self.uri_list = []
         self.amount = 0
 
     def add(self, uri, name):
+        """
+        URI and name are saved. Since no name was found so far, this method
+        will not be implemented for the time being...
+        """
+        pass
+
+    def add_uri(self, uri):
+        """
+        URI is stored in list (the list is appended) and the variable amount
+        is incremented
+        """
         self.amount += 1
         self.uri_list.append(uri)
         self.uri_list.append(name)
     
     def find_all(self):
-        print("start")
+        """
+        This method will list all found connections or links and their
+        names.
+        Adding a specific link will be interactive.
+        """
+        #TODO: specify above
+
+        print("Type in:")
+        print("b - blink, y - add to list, n (else) - do not add to list")
         cflib.crtp.init_drivers()
         available = cflib.crtp.scan_interfaces()
         for i in available:
-            self.add(i[0], i[1])
             print "InterfacewithURI [%s] found, name [%s]" % (i[0],i[1])
+            self.add_uri(i[0], i[1])
         print("end") 
 
     def find_add_all(self):
-        pass
+        """
+        This method will list all found connections or links and their
+        names and add all URIs to the uri-list.
+        Adding a specific link will not be possible here.
+        """
+        print("start find_add_all")
+        cflib.crtp.init_drivers()
+        available = cflib.crtp.scan_interfaces()
+        for i in available:
+            print "InterfacewithURI [%s] found, name [%s]" % (i[0],i[1])
+            self.add_uri(i[0])
+        print("end") 
 
     def print_all(self):
-        print "start print_all"
+        """
+        Print all links stored in the list of URIs
+        """
+        print "Saved in list of URIs:"
         for i in xrange(self.amount):
-            print "Found: [%s]" % (self.uri_list[i])
+            print "[%s] : [%s]" % (i, self.uri_list[i])
         print "end"
 
 
