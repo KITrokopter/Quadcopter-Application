fcb05d73a43f18b627db56eeaa7eec597616e1b9
linoba <dominik.kiefer@t-online.de>
2014-01-29 10:07:55 +0100

    Test for yaw and thrust now working

--- src/test.py
+++ src/test.py
@@ -25,36 +25,92 @@ class CrazyDemo(object):
         self.cf = crazyflie.Crazyflie()
         self.cf.connectSetupFinished.add_callback(self.connected)
         self.cf.disconnected.add_callback(self.disconnected)
-        self.cf.open_link("radio://0/11/250K")
-        # self.cf.open_link("debug://0/0")
+        self.cf.open_link("radio://0/10/250K")
         self.stopevent = threading.Event()
         self.sp = SendPoints(self.cf, self.stopevent, self.point)
 
     def start(self):
         import sys
         range = 350
-        factor = 1000
+        factor = 1024
+        yawfactor = 5
         current = INITIAL_THRUST
+        last_char = "0"
+        alternate = 0   # Counter for alternating between d-u-d-u-...
+        same = 0        # Counter for counting the same input d-d-d-...
+
+        # u Up
+        # d Down
+        # e Exit
+        # t Test (yaw goes up in steps and is set to zero at the end)
+        # q Yaw to negative
+        # w Yaw to positive (clockwise turning from view on top)
         self.sp.start()
-        #for i in xrange(range):
-            # self.point['thrust'] = 0
-            #self.point['thrust'] = INITIAL_THRUST + (factor * i)
-            #time.sleep(0.01)
-        self.point['thrust'] = self.point['thrust'] + 20000
+        self.point['thrust'] = INITIAL_THRUST
+        self.point['yawrate'] = INITIAL_YAWRATE
         while 1:
             ch = sys.stdin.read(1)    
             if ch == "u":
+                if last_char == "d":
+                    alternate += 1
+                    same = 0
+                elif last_char == "u":
+                    alternate = 0
+                    same += 1
                 self.point['thrust'] = self.point['thrust'] + factor
-            elif ch == "e":
-                self.point['thrust'] = self.point['thrust'] - (3*factor)
-                time.sleep(0.5)
-                self.point['thrust'] = self.point['thrust'] - (3*factor)
-                time.sleep(0.5)
-                self.point['thrust'] = self.point['thrust'] - (3*factor)
-                time.sleep(0.5)
-                self.stop();
+                last_char = "u"
             elif ch == "d":
+                if last_char == "u":
+                    alternate += 1
+                    same = 0
+                elif last_char == "d":
+                    alternate = 0
+                    same += 1
                 self.point['thrust'] = self.point['thrust'] - factor
+                last_char = "d"
+            elif ch == "e":
+                #self.point['thrust'] = self.point['thrust'] - (3*factor)
+                #time.sleep(0.5)
+                #self.point['thrust'] = self.point['thrust'] - (3*factor)
+                #time.sleep(0.5)
+                #self.point['thrust'] = self.point['thrust'] - (3*factor)
+                #time.sleep(0.5)
+                self.point['thrust'] = INITIAL_THRUST
+                time.sleep(0.5)
+                alternate = 0
+                same = 0
+                factor = 1024
+                self.stop();
+            elif ch == "y":
+                for i in xrange(8):
+                    self.point['yawrate'] += yawfactor
+                    print self.point
+                    time.sleep(1.0)
+                #self.point['yawrate'] = self.point['yawrate'] + yawfactor
+                self.point['yawrate'] = INITIAL_YAWRATE
+                alternate = 0
+                same = 0
+            elif ch == "w": #positive yaw
+                self.point['yawrate'] = self.point['yawrate'] + yawfactor
+            elif ch == "q": #negative yaw
+                self.point['yawrate'] = self.point['yawrate'] - yawfactor
+            elif ch == "t":
+                factor = 1000
+                for i in xrange (33):
+                    self.point['thrust'] += factor
+                    print self.point
+                    time.sleep(0.08)
+
+                factor = 1024
+
+            if alternate > 3:
+                factor = factor / 2
+                alternate = 0
+            elif same > 3:
+                if factor < 500:
+                    factor *= 2
+                    same = 0
+            print "last char %c" % last_char
             print self.point
             time.sleep(0.05)
 
@@ -80,7 +136,7 @@ class CrazyDemo(object):
     def stabilizer_data(self, data):
         #print 'roll', data['stabilizer.roll']
         #print 'pitch', data['stabilizer.pitch']
-	print "hello"	
+	    print "hello"	
 
     def stop(self):
         print 'closing link'
